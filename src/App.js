b12c9c5acc8b3c603810cdff064c14eddff448c1
Miriam Martinez <miriam.martinez.mc@gmail.com>
2021-04-18 18:32:53 +0200

    Add navigations paths

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router,
+  Switch,
+  Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import StudiesPage from './pages/StudiesPage'
+import StudyPage from './pages/StudyPage'
+import FormsPage from './pages/FormsPage'
+import FormPage from './pages/FormPage'
+import DataPage from './pages/DataPage'
+import NotFoundPage from './pages/NotFoundPage'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/studies">
+              <StudiesPage />
+            </Route>
+            <Route path="/study/:studyName">
+              <StudyPage />
+            </Route>
+            <Route path="/forms">
+              <FormsPage />
+            </Route>
+            <Route path="/form/:formName">
+              <FormPage />
+            </Route>
+            <Route path="/data">
+              <DataPage />
+            </Route>
+            <Route>
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </Router>
+  )
 }
 
-export default App;
+export default App
