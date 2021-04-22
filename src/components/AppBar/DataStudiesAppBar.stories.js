178a478dcb95d996b3c6176124282595015b1b63
joanTrave <jtravego7@alumnes.ub.edu>
2021-04-22 20:24:06 +0200

    DataStudiesAppBar component with its own storybook

---
+++ src/components/AppBar/DataStudiesAppBar.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DataStudiesAppBar from "./DataStudiesAppBar";
+import 'typeface-roboto'
+
+export default {
+    title: "DataStudiesAppBar",
+    component: DataStudiesAppBar
+}
+
+export const ModalWithAllArgs = () => (
+    <DataStudiesAppBar/>
+    )
