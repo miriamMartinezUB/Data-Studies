8d95fb3a4a43f219253415b8c572b66f7049230f
joanTrave <jtravego7@alumnes.ub.edu>
2021-04-29 18:56:23 +0200

    Create Icon Button

---
+++ src/components/IconButton/DataStudiesIconButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {IconButton} from '@material-ui/core'
+import {iconsByName} from "../../constants/icons";
+import Button from "@material-ui/core/Button";
+
+
+const DataStudiesIconButton = ({name, color, size, background, onClick}) => {
+    return (
+        <IconButton
+            aria-label={name}
+            color={color}
+            size={size}
+            style={{backgroundColor: background}}
+            onClick={onClick}
+        >
+            {iconsByName[name]}
+        </IconButton>
+    )
+}
+
+DataStudiesIconButton.propTypes = {
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string,
+    size: PropTypes.string,
+    background: PropTypes.string,
+    onClick: PropTypes.func
+}
+
+export default DataStudiesIconButton
---
+++ src/components/IconButton/DataStudiesIconButton.stories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import DataStudiesIconButton from "./DataStudiesIconButton";
+import {nameIcons} from "../../constants/icons";
+import {COLOR_BLUE} from "../../constants/colors";
+import DataStudiesButton from "../Button";
+
+
+export default {
+    title: "DataStudiesIconButton",
+    component: DataStudiesIconButton
+}
+
+
+export const IconButtonWithAllArgs = () => (
+    <DataStudiesIconButton
+        name={nameIcons.USER}
+        color={"secondary"}
+        size={"medium"}
+        background={COLOR_BLUE}
+        onClick={() => {
+            window.location.href='/studies'
+        }}
+    />
+)
