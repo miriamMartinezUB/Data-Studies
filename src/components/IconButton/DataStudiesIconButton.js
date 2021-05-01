8680b0cb73cd4cd753dd11b926fdb287b77c31d0
Miriam Martinez <miriam.martinez.mc@gmail.com>
2021-05-01 13:28:29 +0200

    Add isDisabled in iconbutton

--- src/components/IconButton/DataStudiesIconButton.js
+++ src/components/IconButton/DataStudiesIconButton.js
@@ -4,13 +4,15 @@ import {IconButton} from '@material-ui/core'
 import {iconsByName} from "../../constants/icons";
 
 
-const DataStudiesIconButton = ({name, color, size, background, onClick}) => {
+const DataStudiesIconButton = ({name, color, size, background, onClick, isDisabled}) => {
+    if(isDisabled==null) isDisabled=false
     return (
         <IconButton
             aria-label={name}
             color={color}
             size={size}
             style={{backgroundColor: background}}
+            disabled={isDisabled}
             onClick={onClick}
         >
             {iconsByName[name]}
@@ -23,7 +25,8 @@ DataStudiesIconButton.propTypes = {
     color: PropTypes.string,
     size: PropTypes.string,
     background: PropTypes.string,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    isDisabled:PropTypes.bool,
 }
 
 export default DataStudiesIconButton
