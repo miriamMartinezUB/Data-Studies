8dba53e521e807a64a8c6c72f5b792ded04ea676
Miriam Martinez <miriam.martinez.mc@gmail.com>
2021-05-01 13:31:35 +0200

    Fill screens

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import { Link as RouterLink } from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import DataStudiesIconButton from "../components/IconButton";
+import {nameIcons} from "../constants/icons";
+import 'typeface-roboto'
 
 const NotFoundPage = () => {
     return (
-        <h1>Not implemented yet</h1>
+        <Grid container
+              direction="column"
+              justify="center"
+              className="full">
+            <div className="highlight">
+                <Grid item container xs={12}
+                      justify="center"
+                      alignItems="center">
+                    <Grid item>
+                        <DataStudiesIconButton name={nameIcons.SAD} color={"disabled"} isDisabled={true} />
+                    </Grid>
+                    <Grid item
+                          container
+                          direction="column"
+                          justify="center"
+                          alignItems="center">
+                        <Typography variant="h4" color="inherit">
+                            404 | La página no existe
+                        </Typography>
+                        <Link color="inherit"
+                              aria-label="menu"
+                              aria-label="menu"
+                              component={RouterLink}
+                              to="/">
+                            Volver al inicio
+                        </Link>
+                    </Grid>
+                </Grid>
+            </div>
+        </Grid>
     )
 }
 
